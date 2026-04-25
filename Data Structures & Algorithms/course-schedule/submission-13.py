6cf10882401691aaf039382323aa147f6967498e
Sanjay DV <55549923+sdv1708@users.noreply.github.com>
2026-04-25 18:58:51 -0500

    Add: course-schedule - submission-13

---
+++ Data Structures & Algorithms/course-schedule/submission-13.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj_map = defaultdict(list)
+        for cou, pre in prerequisites:  
+            adj_map[cou].append(pre) # course -> prereq mapping 
+
+        visited = set()
+
+        def dfs(course, visiting):  
+            if course in visiting: 
+                return False    
+            
+            if course in visited: 
+                return True
+            
+            visiting.add(course)
+
+            for pre in adj_map[course]: 
+                if not dfs(pre, visiting): 
+                    return False 
+            visited.add(course)
+            visiting.remove(course)
+            return True
+
+        visiting = set()
+        for cou in range(numCourses):
+            if not dfs(cou, visiting): 
+                return False 
+
+        return True 
+
+
+            
+            
+
+
+
+        

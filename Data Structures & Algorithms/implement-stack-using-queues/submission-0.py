76ff0a9b4fcf181f0076a9838886aa43dfe21bd5
Sanjay DV <55549923+sdv1708@users.noreply.github.com>
2026-05-16 02:15:18 -0500

    Add: implement-stack-using-queues - submission-0

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-0.py
@@ -0,0 +1,36 @@
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque()
+        
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+        for _ in range(len(self.queue) - 1): 
+            self.queue.append(self.queue.popleft())
+        # # EXAMPLE TRACE:
+        # Initial Queue: [1, 2] (Front is 1)
+        # 1. push(3) -> Queue becomes [1, 2, 3]
+        # 2. Rotate (Loop runs 2 times):
+        #    - Pop 1, Append 1 -> [2, 3, 1]
+        #    - Pop 2, Append 2 -> [3, 1, 2]
+        # Result: [3, 1, 2] -> 3 is now at the front (Top of Stack)
+        
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+        
+
+    def top(self) -> int:
+        return self.queue[0]
+        
+    def empty(self) -> bool:
+        return len(self.queue) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
